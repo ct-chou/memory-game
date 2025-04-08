985e06f71cd699309cd32ca0bbd431c0a1a85da7
ct-chou <63345621+ct-chou@users.noreply.github.com>
2025-04-07 20:41:46 -0400

    Setup header

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0);
+  // const [highScore, setHighScore] = useState(0);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <header>
+        <h1>Pokemon Memory Game</h1>
+        <div id='scoreboard'>
+          <div className="card">
+              Current score is {score}
+          </div>
+          <div className="card">
+              High score is {score}
+          </div>
+        </div>
+      </header>
+      
+      <p className="instructions">
+        Select a card to earn points. If you select a card that has already been selected, you lose. 
       </p>
     </>
   )
